87b9fa26379dabc3b5247d897fb6f92c515de6ce
Rodrigo Campos <rodrigo@webdevel.cl>
2015-07-27 17:50:04 -0300

    products admin panel added

---
+++ shopping/application/views/admin/templates/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+        <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>">
+        <link rel="stylesheet" href="<?php echo base_url("assets/css/jquery.Jcrop.min.css"); ?>" type="text/css" />
+        <link rel="stylesheet" href="<?php echo base_url("assets/css/styles.css"); ?>">
+
+
+        <title>Panel de Administración - Bessalle</title>
+
+    </head>
+    <body>
+
+<div class="container">
+
+
+
