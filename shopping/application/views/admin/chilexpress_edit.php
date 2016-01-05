4246c7717d3511a55bd42e51aec97f1fdf3cb071
Rodrigo Campos <rodrigo@webdevel.cl>
2016-01-05 12:41:08 -0300

    corregido error

--- shopping/application/views/admin/chilexpress_edit.php
+++ shopping/application/views/admin/chilexpress_edit.php
@@ -1,11 +1,11 @@
 <div>
   <ul class="nav nav-tabs right-to-left" role="tablist">
-    <li role="presentation" class="active"><a href="#" aria-controls="categorias" role="tab">Memphis</a></li>
+    <li role="presentation" class="active"><a href="#" aria-controls="categorias" role="tab">Chilexpress</a></li>
   </ul>
 
   <div class="tab-content padding-top-30">
     <div role="tabpanel" class="tab-pane active">
-        <?php echo form_open('admin/transport/memphis/'.$memphis['id']) ?>
+        <?php echo form_open('admin/transport/chilexpress/'.$chilexpress['id']) ?>
             <div class="panel panel-default">
 
                 <div class="panel-heading">
@@ -16,7 +16,7 @@
                         <div class="col-md-12">
                             <div class="form-group">
                                 <label for="name">Nombre</label>
-                                <input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo $memphis['nombre']; ?>"><span class="error"><?php echo form_error('nombre'); ?></span>
+                                <input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo $chilexpress['nombre']; ?>"><span class="error"><?php echo form_error('nombre'); ?></span>
                             </div>
                         </div>
                     </div>
@@ -24,37 +24,37 @@
                         <div class="col-md-2">
                             <div class="form-group">
                                 <label for="name">1K50</label>
-                                <input type="text" class="form-control" id="1K50" name="1K50" value="<?php echo $memphis['1K50']; ?>"><span class="error"><?php echo form_error('1K50'); ?></span>
+                                <input type="text" class="form-control" id="1K50" name="1K50" value="<?php echo $chilexpress['1K50']; ?>"><span class="error"><?php echo form_error('1K50'); ?></span>
                             </div>
                         </div>
                         <div class="col-md-2">
                             <div class="form-group">
                                 <label for="name">3K</label>
-                                <input type="text" class="form-control" id="3K" name="3K" value="<?php echo $memphis['3K']; ?>"><span class="error"><?php echo form_error('3K'); ?></span>
+                                <input type="text" class="form-control" id="3K" name="3K" value="<?php echo $chilexpress['3K']; ?>"><span class="error"><?php echo form_error('3K'); ?></span>
                             </div>
                         </div>
                         <div class="col-md-2">
                             <div class="form-group">
                                 <label for="name">6K</label>
-                                <input type="text" class="form-control" id="6K" name="6K" value="<?php echo $memphis['6K']; ?>"><span class="error"><?php echo form_error('6K'); ?></span>
+                                <input type="text" class="form-control" id="6K" name="6K" value="<?php echo $chilexpress['6K']; ?>"><span class="error"><?php echo form_error('6K'); ?></span>
                             </div>
                         </div>
                         <div class="col-md-2">
                             <div class="form-group">
                                 <label for="name">10K</label>
-                                <input type="text" class="form-control" id="10K" name="10K" value="<?php echo $memphis['10K']; ?>"><span class="error"><?php echo form_error('10K'); ?></span>
+                                <input type="text" class="form-control" id="10K" name="10K" value="<?php echo $chilexpress['10K']; ?>"><span class="error"><?php echo form_error('10K'); ?></span>
                             </div>
                         </div>
                         <div class="col-md-2">
                             <div class="form-group">
                                 <label for="name">15K</label>
-                                <input type="text" class="form-control" id="15K" name="15K" value="<?php echo $memphis['15K']; ?>"><span class="error"><?php echo form_error('15K'); ?></span>
+                                <input type="text" class="form-control" id="15K" name="15K" value="<?php echo $chilexpress['15K']; ?>"><span class="error"><?php echo form_error('15K'); ?></span>
                             </div>
                         </div>
                         <div class="col-md-2">
                             <div class="form-group">
                                 <label for="name">ADIC</label>
-                                <input type="text" class="form-control" id="ADIC" name="ADIC" value="<?php echo $memphis['ADIC']; ?>"><span class="error"><?php echo form_error('ADIC'); ?></span>
+                                <input type="text" class="form-control" id="ADIC" name="ADIC" value="<?php echo $chilexpress['ADIC']; ?>"><span class="error"><?php echo form_error('ADIC'); ?></span>
                             </div>
                         </div>
                     </div>
