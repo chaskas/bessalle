cf9747573ea8716483220944156a16ecf8f593cf
Rodrigo Campos <rodrigo@webdevel.cl>
2016-01-07 16:28:39 -0300

    correcion

--- shopping/application/views/admin/chilexpress_edit.php
+++ shopping/application/views/admin/chilexpress_edit.php
@@ -9,7 +9,7 @@
             <div class="panel panel-default">
 
                 <div class="panel-heading">
-                    <h3 class="panel-title">Editar Memphis</h3>
+                    <h3 class="panel-title">Editar Chilexpress</h3>
                 </div>
                 <div class="panel-body">
                     <div class="row">
